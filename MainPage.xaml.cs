afe0c1f675864348d14fc37c499f2846ac2ea58d
AndrewStoddard <acs8182000@gmail.com>
2020-11-11 17:27:22 -0500

    Made a version using a gridview

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System.Linq;
-using Windows.System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -15,25 +14,47 @@ namespace NumbrixGame
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region Data members
+
+        private readonly IList<TextBox> cells;
+
+        #endregion
+
         #region Constructors
 
         public MainPage()
         {
             this.InitializeComponent();
-            this.createGameBoard(TempWidth, TempHeight);
+            this.cells = this.createGridGameBoard(this.TempWidth, this.TempHeight);
         }
 
         #endregion
 
         #region Methods
 
+        private IList<TextBox> createGridGameBoard(int width, int height)
+        {
+            var cells = new List<TextBox>();
+            for (var i = 1; i <= height; i++)
+            {
+                for (var j = 1; j <= width; j++)
+                {
+                    var cell = this.createCell();
+
+                    cell.Text = (j + width * (i - 1)).ToString();
+
+                    cells.Add(cell);
+                }
+            }
+
+            return cells;
+        }
+
         private void createGameBoard(int width, int height)
         {
             var parentStackPanel = new StackPanel();
             parentStackPanel.BorderBrush = new SolidColorBrush(Colors.Blue);
             parentStackPanel.BorderThickness = new Thickness(2);
-            this.parentGrid.VerticalAlignment = VerticalAlignment.Center;
-            this.parentGrid.HorizontalAlignment = HorizontalAlignment.Center;
 
             this.parentGrid.Children.Add(parentStackPanel);
 
@@ -60,29 +81,20 @@ namespace NumbrixGame
             }
         }
 
-        private void OnKeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            var textBox = sender as TextBox;
-
-            if (e.Key == VirtualKey.Left)
-            {
-            }
-            else if (e.Key == VirtualKey.Right)
-            {
-            }
-        }
-
         private TextBox createCell()
         {
             var newTextBox = new TextBox();
             newTextBox.Width = 50;
+            newTextBox.BorderBrush = new SolidColorBrush(Colors.Transparent);
+            newTextBox.BorderThickness = new Thickness(0);
             newTextBox.Height = 50;
             newTextBox.FontSize = 24;
             newTextBox.TextAlignment = TextAlignment.Center;
             newTextBox.BeforeTextChanging += this.NewCell_BeforeTextChanging;
             newTextBox.GotFocus += this.NewTextBoxOnGotFocus;
-            newTextBox.SelectionHighlightColor = new SolidColorBrush(Colors.Transparent);
-            newTextBox.KeyDown += this.OnKeyDown;
+            newTextBox.SelectionHighlightColor = new SolidColorBrush(Colors.DarkGray);
+            newTextBox.FocusVisualPrimaryBrush = new SolidColorBrush(Colors.DarkGray);
+            newTextBox.FocusVisualSecondaryBrush = new SolidColorBrush(Colors.Black);
             return newTextBox;
         }
 
@@ -98,7 +110,8 @@ namespace NumbrixGame
             if (!string.IsNullOrEmpty(args.NewText))
             {
                 args.Cancel = args.NewText.Any(c => !char.IsDigit(c)) ||
-                              int.Parse(args.NewText) > TempHeight * TempWidth || args.NewText.StartsWith("0");
+                              int.Parse(args.NewText) > this.TempHeight * this.TempWidth ||
+                              args.NewText.StartsWith("0");
             }
         }
 
@@ -106,8 +119,8 @@ namespace NumbrixGame
 
         #region Constants
 
-        private const int TempWidth = 9;
-        private const int TempHeight = 9;
+        private readonly int TempWidth = 9;
+        private readonly int TempHeight = 9;
 
         #endregion
     }
