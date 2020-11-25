bd7f246daa17139e68ed41c3b9551638c782744c
AndrewStoddard <acs8182000@gmail.com>
2020-11-24 20:14:13 -0500

    Added a ScoreBoard and PlayerScore model.

---
+++ Model/NumbrixPlayerScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumbrixGame.Model
+{
+    public class NumbrixPlayerScore
+    {
+        #region Properties
+
+        public string Username { get; set; }
+        public TimeSpan TimeTaken { get; set; }
+        public int PuzzleNumber { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public NumbrixPlayerScore(string username, TimeSpan timeTaken, int puzzleNumber)
+        {
+            this.Username = username;
+            this.TimeTaken = timeTaken;
+            this.PuzzleNumber = puzzleNumber;
+        }
+
+        #endregion
+    }
+}
---
+++ Model/NumbrixScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace NumbrixGame.Model
+{
+    public class NumbrixScoreBoard
+    {
+        #region Properties
+
+        public IList<NumbrixPlayerScore> PlayerScores { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public NumbrixScoreBoard()
+        {
+            this.PlayerScores = new List<NumbrixPlayerScore>();
+        }
+
+        #endregion
+    }
+}
