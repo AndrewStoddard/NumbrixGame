0429ed4759b29d23e72bf178db6212f6f0f9126b
AndrewStoddard <acs8182000@gmail.com>
2020-11-24 20:32:55 -0500

    Implemented view models for sccoreboard and player score

---
+++ ViewModel/NumbrixPlayerScoreViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using NumbrixGame.Model;
+
+namespace NumbrixGame.ViewModel
+{
+    public class NumbrixPlayerScoreViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public NumbrixPlayerScore Model { get; set; }
+
+        public string Username
+        {
+            get => this.Model.Username;
+            set
+            {
+                this.Model.Username = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Username)));
+            }
+        }
+
+        public TimeSpan TimeTaken
+        {
+            get => this.Model.TimeTaken;
+            set
+            {
+                this.Model.TimeTaken = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.TimeTaken)));
+            }
+        }
+
+        public int PuzzleNumber
+        {
+            get => this.Model.PuzzleNumber;
+            set
+            {
+                this.Model.PuzzleNumber = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.PuzzleNumber)));
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public NumbrixPlayerScoreViewModel(string username, TimeSpan timeTaken, int puzzleNumber)
+        {
+            this.Model = new NumbrixPlayerScore(username, timeTaken, puzzleNumber);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
---
+++ ViewModel/NumbrixScoreBoardViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace NumbrixGame.ViewModel
+{
+    public class NumbrixScoreBoardViewModel : INotifyPropertyChanged
+    {
+        #region Data members
+
+        private IList<NumbrixPlayerScoreViewModel> playerScores;
+
+        #endregion
+
+        #region Properties
+
+        public IList<NumbrixPlayerScoreViewModel> PlayerScores
+        {
+            get => this.playerScores;
+            set
+            {
+                this.playerScores = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.playerScores)));
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public NumbrixScoreBoardViewModel()
+        {
+            this.PlayerScores = new List<NumbrixPlayerScoreViewModel>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
